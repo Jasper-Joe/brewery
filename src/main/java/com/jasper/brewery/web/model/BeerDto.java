63280de8d9e1d3e25ed52a487854992357892ef4
jasper <jasperzhou1129@gmail.com>
2021-03-13 17:09:50 -0500

    validation

--- src/main/java/com/jasper/brewery/web/model/BeerDto.java
+++ src/main/java/com/jasper/brewery/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 @Data
@@ -12,9 +16,16 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID id;
+
+    @NotBlank
     private String beerName;
+
+    @NotBlank
     private String beerStyle;
+
+    @Positive
     private Long upc;
 
 }
