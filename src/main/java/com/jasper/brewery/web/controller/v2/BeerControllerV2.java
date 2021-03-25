f3860b05d64473ced68af119bed5c8a979628648
jasper <jasperzhou1129@gmail.com>
2021-03-24 22:58:19 -0400

    lombok

--- src/main/java/com/jasper/brewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/com/jasper/brewery/web/controller/v2/BeerControllerV2.java
@@ -4,6 +4,9 @@ import com.jasper.brewery.services.BeerService;
 import com.jasper.brewery.services.v2.BeerServiceV2;
 import com.jasper.brewery.web.model.BeerDto;
 import com.jasper.brewery.web.model.v2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +19,17 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+@Slf4j
+@RequiredArgsConstructor
 @Validated
 @RequestMapping("/api/v2/beet")
 @RestController
 public class BeerControllerV2 {
     private final BeerServiceV2 beerService;
 
-    public BeerControllerV2(BeerServiceV2 beerService) {
-        this.beerService = beerService;
-    }
+//    public BeerControllerV2(BeerServiceV2 beerService) {
+//        this.beerService = beerService;
+//    }
 
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDtoV2> getBeer(@NotNull @PathVariable("beerId") UUID beerId) {
@@ -33,8 +38,8 @@ public class BeerControllerV2 {
 
     @PostMapping
     public ResponseEntity handlePost(@Valid @NotNull @RequestBody BeerDtoV2 beerDto) {
-        BeerDtoV2 savedDto = beerService.saveNewBeer(beerDto);
-        HttpHeaders headers = new HttpHeaders();
+        val savedDto = beerService.saveNewBeer(beerDto);
+        val headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
